3393de4aafaa42ab87d7616c4efd33775383e727
juanroyo <juanroyofullstack@gmail.com>
2026-02-01 21:30:46 +0100

    add: homepage domain in folder as feature folder design

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,12 @@
 import Header from './shared/components/Header';
-import { Box, Container, Typography } from '@mui/material';
-import bugsBunny from './assets/bugsBunnyFace.png';
+import HomePageView from './features/homepage/views/HomePageView';
 import './App.css';
 
 function App() {
   return (
     <>
       <Header />
-      <Container component="main" maxWidth="lg" sx={{ py: 4 }}>
-        <Typography variant="h4" component="h2" gutterBottom>
-          Welcome to My Application
-        </Typography>
-        <Typography variant="h4" component="h3" gutterBottom>
-          Add your content here
-        </Typography>
-        <Box
-          alt="Bugs Bunny"
-          className="logo"
-          component="img"
-          src={bugsBunny}
-          sx={{ width: 200, height: 'auto', borderRadius: 2 }}
-        />
-      </Container>
+      <HomePageView />
     </>
   );
 }
---
+++ src/features/homepage/views/HomePageView.tsx
@@ -0,0 +1,24 @@
+import { Box, Container, Typography } from '@mui/material';
+import bugsBunny from '../../../assets/bugsBunnyFace.png';
+
+const HomePageView = () => {
+  return (
+    <Container component="main" maxWidth="lg" sx={{ py: 4 }}>
+      <Typography variant="h4" component="h2" gutterBottom>
+        Welcome to My Application
+      </Typography>
+      <Typography variant="h4" component="h3" gutterBottom>
+        Add your content here
+      </Typography>
+      <Box
+        alt="Bugs Bunny"
+        className="logo"
+        component="img"
+        src={bugsBunny}
+        sx={{ width: 200, height: 'auto', borderRadius: 2 }}
+      />
+    </Container>
+  );
+};
+
+export default HomePageView;
