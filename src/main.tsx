7f5bca9f78f41acb1d16750581b064041ba5ba9f
juanroyo <juanroyofullstack@gmail.com>
2026-02-01 18:15:57 +0100

    add: redux and auto formatt

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,17 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import { Provider } from 'react-redux';
+import { store } from './store';
 import { BrowserRouter } from 'react-router-dom';
-import './index.css'
-import App from './App.tsx'
+import './index.css';
+import App from './App.tsx';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <BrowserRouter>
-      <App />
+      <Provider store={store}>
+        <App />
+      </Provider>
     </BrowserRouter>
   </StrictMode>,
-)
+);
